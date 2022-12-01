fae558d373ad5f4930be6bed4b1e64ab6b5df549
maithili-10 <angajalamaithiliraj@gmail.com>
2022-12-02 00:42:17 +0400

    created graphql,express server,connected to mongodb database

---
+++ app.js
@@ -0,0 +1,72 @@
+const express= require('express');
+const bodyParser=require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const {buildSchema}=require('graphql');
+const mongoose=require('mongoose');
+
+
+const app=express();
+const events=[];
+
+app.use(bodyParser.json());
+
+// app.get('/',(req,res,next)=>{
+//     res.send("Hello World");
+// })
+
+app.use("/graphql",graphqlHTTP({
+    schema:buildSchema(`
+
+    type Event{
+        _id:ID!
+        title:String!
+        description:String!
+        price:Float!
+        date:String!
+    }
+input EventInput{
+    title:String!
+    description:String!
+    price:Float!
+    date:String!
+}
+    type RootQuery{
+        events:[Event!]!
+
+    }
+    type RootMutation{
+            createEvent(eventInput:EventInput ):Event
+    }
+    schema{
+        query:RootQuery
+        mutation:RootMutation
+    }
+    `),
+    rootValue:{
+        events:()=>{
+            return events;
+
+        },
+        createEvent:(args)=>{
+            const event={
+                _id:Math.random().toString(),
+                title:args.eventInput.title,
+                description:args.eventInput.description,
+                price:+args.eventInput.price,
+                date:args.eventInput.date
+            };
+            events.push(event);
+            return event;
+        }
+    },
+    graphiql:true
+
+}));
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.zmh44ee.mongodb.net/?retryWrites=true&w=majority`)
+.then(()=>{
+    app.listen(5000);
+}).catch(err=>{
+console.log(err);
+})
+
