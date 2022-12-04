66d3a7a36c3ca9fb616601f5254510f77e230209
maithili-10 <angajalamaithiliraj@gmail.com>
2022-12-04 19:55:51 +0400

    events page

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,39 @@
 import React,{Component} from 'react';
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Events.css'
 export default class EventsPage extends Component{
+    state = {
+        creating: false,
+        events: [],
+        isLoading: false,
+        selectedEvent: null
+      };
+      startCreateEventHandler = () => {
+        this.setState({ creating: false });
+      };
+      modalConfirmHandler = () => {
+        this.setState({ creating: false });
+       
+        }
+
+        modalCancelHandler = () => {
+            this.setState({ creating: false, });
+          };
+    
     render(){
         return(
-            <h1>Events Page</h1>
+            <React.Fragment>
+                   {(this.state.creating || this.state.selectedEvent) && <Backdrop />}
+            {/* <Modal title="ADD EVENT" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+                <p>Share your own event</p></Modal>    */}
+                 <div className='events-control'>
+                <p>Share your own events</p>
+<button className='btn' onClick={this.startCreateEventHandler}>Create Event</button>
+            </div>
+            </React.Fragment>
+           
+           
         )
     }
 }
